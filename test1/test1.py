cb02c92074928c37cabb37753804a6ca5ffd258e
TZ-WORKSTATION <TZ-WORKSTATION@TZ-WORKSTATION>
2017-10-02 20:28:23 -0400

    test 2

---
+++ test1/test1.py
@@ -0,0 +1,32 @@
+# Muhammad Tarequzzaman 100954008 SYSC3010A F17 M6
+import pifacedigitalio as pf
+from time import sleep
+
+pf = pf.PiFaceDigital()
+
+#turn on leds
+for i in range(8):
+  print ('Turning on LEDs ' + repr(i+1))
+  pf.output_pins[i].turn_on()
+  sleep(0.5)
+
+for i in range (2):
+  print ('Activating relay '  + repr(i))
+  pf.relays[i].turn_on()
+  sleep(1)
+  print ('Deactivating relay '  + repr(i))
+  pf.relays[i].turn_off()
+  sleep(1)
+
+print('Reading switches')
+
+for i in range (4):
+  print('Switch ' +repr(i+1)+ 'is: '+ repr(pf.switches[i].value))
+
+#turn off  
+for i in range(8):
+  print ('Turning off LEDs ' + repr(i+1))
+  pf.output_pins[i].turn_off()
+  sleep(0.5)
+
+exit()
