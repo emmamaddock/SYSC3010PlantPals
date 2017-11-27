51d60779e5ec01057770156d828088070cce15d9
TZ-WORKSTATION <TZ-WORKSTATION@TZ-WORKSTATION>
2017-11-26 22:22:38 -0500

    # WARNING: head commit changed in the meantime
    Demo piface

--- Rpi_headless/SetupGPIO.py
+++ Rpi_headless/SetupGPIO.py
@@ -6,13 +6,12 @@ Created on Nov 26, 2017
 import RPi.GPIO as GPIO
 
 def setupGPIO():
+    ControlPin = [4,17,27,2,23]
     GPIO.setmode(GPIO.BCM)
-    ControlPin = [4,17,27,22]
+    
 # GPIO pin setting up  
     for pin in ControlPin:
         GPIO.setup(pin,GPIO.OUT)
         GPIO.output(pin,0)
     
-    pin = 23
-GPIO.setup(pin,GPIO.OUT)
-GPIO.output(pin,0)
+  
