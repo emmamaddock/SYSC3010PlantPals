e55475622c587285af27feb9c667e5bc8a4c4f61
m-tareq-cu <m.tareq@users.noreply.github.com>
2017-12-06 16:53:34 -0500

    test

--- Rpi_headless/test/TEST.py
+++ Rpi_headless/test/TEST.py
@@ -5,14 +5,21 @@ Created on Dec 2, 2017
 '''
 import unittest
 import RPi.GPIO as GPIO
+import Read_serial as serial
 
 class Test(unittest.TestCase):
 
     def testGPIO(self):
-        
         pass
-
-
+    def testSerial(self):
+        a = serial.isActive()
+        self.assertEqual(a,0, 'Check The Serial Port ')
+    def testPump(self):
+        self.testGPIO()
+    
+    def testStepperMotor(self):
+        self.testGPIO() 
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testGPIO']
     unittest.main()
